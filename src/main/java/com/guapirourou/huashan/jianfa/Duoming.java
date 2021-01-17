2c55504a3d7dbca910c66ed5c7ba7bf661ae568a
guapirourouzZ <690689726@qq.com>
2021-01-17 17:53:14 +0800

    增加三仙贱

---
+++ src/main/java/com/guapirourou/huashan/jianfa/Duoming.java
@@ -0,0 +1,9 @@
+package com.guapirourou.huashan.jianfa;
+
+public class Duoming {
+    public static void main(String[] args) {
+        System.out.println("夺命三连贱");
+        System.out.println("夺命三连贱");
+        System.out.println("夺命三连贱");
+    }
+}
